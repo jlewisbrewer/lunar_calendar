0bd85c63fa66292e5322a930ac1898071c20b181
Jason Brewer <jbrewer@pdx.edu>
2020-02-13 19:43:50 -0800

    fixed lunar cycles for negative dates. Created some tests. Need more testing. Has console date integration.

--- src/lunar.rs
+++ src/lunar.rs
@@ -3,6 +3,7 @@
 const SYNODIC_MONTH : i64 = 2551443;
 // offset based on new moon on Jan 7, 12:35 PM, 1970
 const SYNODIC_OFFSET : i64 = 606900;
+const NEGATIVE_SYNODIC_OFFSET : i64 = SYNODIC_OFFSET - SYNODIC_MONTH;
 // Value for tuning how exact the full, new, and quarter moon phases are
 // Has to be between 0 and 1
 const TUNING_VALUE : f64 = 0.3;
@@ -10,11 +11,25 @@ const TUNING_VALUE : f64 = 0.3;
 
 
 fn get_phase_value(date: i64) -> f64 {
+    let period = SYNODIC_MONTH / 8;    
+    let mut diff = (date - SYNODIC_OFFSET) % SYNODIC_MONTH;
+    
+    if date < SYNODIC_OFFSET {
+        // If the date occured before epoch, we have to deal with negative
+        // numbers.
+        if date >= NEGATIVE_SYNODIC_OFFSET {
+            // This will need some checks
+            diff = (date - NEGATIVE_SYNODIC_OFFSET) % SYNODIC_MONTH;
+        } else if date < NEGATIVE_SYNODIC_OFFSET {
+            // This seems to work
+            diff = SYNODIC_MONTH - ((date - NEGATIVE_SYNODIC_OFFSET) * -1 % SYNODIC_MONTH);
+        } else {
+            // If the date is less than the offset, then we need to base the
+            // starting point of the new moon cycle to be a month - offset earler.
+            diff = (date + SYNODIC_MONTH - SYNODIC_OFFSET) % SYNODIC_MONTH;
+        }
+    }
 
-    let period = SYNODIC_MONTH / 8;
-    
-    let diff = (date - SYNODIC_OFFSET) % SYNODIC_MONTH;
-    
     diff as f64 / period as f64
 }
 
@@ -71,4 +86,21 @@ pub fn get_lunar_phase(date: i64) {
     println!{"The moon is {:.2}% full.", get_fullness_percentage(phase_var)};
     println!{"{}", get_phase_string(phase_var)};
 
+}
+
+#[test]
+fn test_get_phase_value() {
+    let floor : f64 = 5.0;
+    let ceiling : f64 = 6.0;
+    let date : i64 = 1581567657;
+    let test_case = get_phase_value(date);
+
+    assert!(test_case < ceiling, test_case > floor);
+}
+
+#[test]
+fn test_get_phase_string() {
+    let test_phase_variable : f64 = 5.07;
+    assert_eq!("It is a waning gibbous.".to_string(), 
+        get_phase_string(test_phase_variable));
 }
--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,19 @@ extern crate chrono;
 mod lunar;
 use chrono::prelude::*;
 use std::time::{Instant, SystemTime};
+use std::env;
 
 fn main() {
-    // s is start of new moon, can be divided into 8ths to find the phases
-    // let s : u64 = 2551443;
-    // let p = s / 8;
-    // let so : u64 = 606900;
-    // let n = Instant::now();
-    // let mut sn = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
-    // println!("Now : {:?}", sn);
-    
-    
-    // let diff = ((sn) - so) % s;
-    // println!("{}", diff);
-    // let phase : f64 = (diff as f64 / p as f64);
-    // println!("{}", phase);
-    // // Let's turn this into a percentage
-    // let mut percentage : f64 = 0.0;
-    // if phase > 4.0 {
-    //     percentage = ((8.0 - phase) / 4.0) * 100.0;
-    // }
-    // else {
-    //     percentage = (phase / 4.0) * 100.0;
-    // }
-    // println!("The moon is {:.2}% full", percentage);
 
-    // // Ok so this just finds the phase of the moon based on the base of the era
-    // // To do, we need to make it into constants and integrate it.
-    // // Will need to convert any date to era and then find the moon time
-    
-    // // Need to convert seconds to a date
-    // let today = Local::now();
-    // println!("{}", today);
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 4);
 
-    // //Tests
+    let year : i32 = args[3].parse().unwrap();
+    let month : u32 = args[1].parse().unwrap();
+    let day : u32 = args[2].parse().unwrap();
 
-    let dt = Utc.ymd(1955, 4, 25).and_hms(0, 0, 0);
+    println!{"{:?}", args};
+    let dt = Utc.ymd(year, month, day).and_hms(17, 54, 1);
     println!{"Utc: {:?}", dt.timestamp()};
 
     lunar::get_lunar_phase(dt.timestamp());
